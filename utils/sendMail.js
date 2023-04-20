d4b0b1658468143d3d4e58874b5a4455a1979bd5
yukti-sachdeva <yukti.s@antino.io>
2023-04-20 15:14:31 +0530

    maerging staging

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,35 +1,24 @@
-
 const nodeMailer = require("nodemailer");
 
-
-
 const sendEmail = async (options) => {
- const transporter = nodeMailer.createTransport({
- host: 'smtp.gmail.com',
-  port: 465,
- service: 'gmail',
- secure: true,
- auth: {
- user: 'yukti845@gmail.com',
-  pass: 'eqpakeumvrdxogsc'
- },
- });
+    const transporter = nodeMailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        service: 'gmail',
+        secure: true,
+        auth: {
+            user: 'yukti845@gmail.com',
+            pass: 'eqpakeumvrdxogsc'
+        },
+    });
 
+    const mailOptions = {
+        from: process.env.SMTP_MAIL,
+        to: options.email,
+        subject: options.subject,
+        html: options.message,
+    };
 
-
- const mailOptions = {
- from: process.env.SMTP_MAIL,
- to: options.email,
-  subject: options.subject,
- html: options.message,
- };
-
-
-
-await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
 };
-<<<<<<< HEAD
 module.exports = sendEmail;
-=======
-module.exports = sendEmail;
->>>>>>> f7eeef9e27c21bc98b15dfb927277002af02f640
